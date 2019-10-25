8514f84a9a06d33eb3161fcb341562e0bf486094
Kevin Li <kevintli98@gmail.com>
2019-10-25 05:41:35 -0700

    Add argparse to simulator for convenience

--- Simulator_v2/RunSimulation.py
+++ Simulator_v2/RunSimulation.py
@@ -5,8 +5,9 @@ import itertools
 from Garden import Garden
 from Plant import Plant
 from Logger import Event
+import argparse
 
-NUM_TIMESTEPS = 50
+NUM_TIMESTEPS = 40
 NUM_X_STEPS = 50
 NUM_Y_STEPS = 50
 STEP = 1
@@ -17,19 +18,19 @@ PLANT_COLORS = [(.49, .99, 0), (.13, .55, .13), (0, .39, 0), (0, .65, .15)]
 
 # Creates different color plants in random locations
 def get_random_plants():
-    np.random.seed(28506631)
-    plants = []
-    for c in PLANT_COLORS:
-        x_locations = np.random.randint(1, NUM_X_STEPS - 1, (PLANTS_PER_COLOR, 1))
-        y_locations = np.random.randint(1, NUM_Y_STEPS - 1, (PLANTS_PER_COLOR, 1))
-        locations = np.hstack((x_locations, y_locations))
-        plants.extend([Plant(row, col, color=c) for row, col in locations])
-    return plants
-    # np.random.seed(38572912)
-    # return [Plant(20, 20, color='g'), Plant(23, 23, color='b'), Plant(22, 22, color='k'), Plant(40, 40, color='c')]
+    # np.random.seed(28506631)
+    # plants = []
+    # for c in PLANT_COLORS:
+    #     x_locations = np.random.randint(1, NUM_X_STEPS - 1, (PLANTS_PER_COLOR, 1))
+    #     y_locations = np.random.randint(1, NUM_Y_STEPS - 1, (PLANTS_PER_COLOR, 1))
+    #     locations = np.hstack((x_locations, y_locations))
+    #     plants.extend([Plant(row, col, color=c) for row, col in locations])
+    # return plants
+    np.random.seed(38572912)
+    return [Plant(20, 20, color='g'), Plant(23, 23, color='b'), Plant(22, 22, color='k'), Plant(40, 40, color='c')]
 
 # Test run of simulation
-def run_simulation():
+def run_simulation(args):
     # np.random.seed(38572912)
     plants = get_random_plants()
 
@@ -60,18 +61,24 @@ def run_simulation():
             plots.append(circleplot)
         frames.append(plots)
 
-    for event_type in Event:
-        plt.figure()
-        for plant in plants:
-            plt.plot(range(NUM_TIMESTEPS), garden.logger.get_data(event_type, plant.id), color=plant.color)
-        plt.plot(range(NUM_TIMESTEPS), garden.logger.get_data(event_type, "Control"), color='r', linestyle='--')
-        plt.title(event_type.value)
-    plt.show()
+    if args.display == 'p':
+        for event_type in Event:
+            plt.figure()
+            for plant in plants:
+                plt.plot(range(NUM_TIMESTEPS), garden.logger.get_data(event_type, plant.id), color=plant.color)
+            plt.plot(range(NUM_TIMESTEPS), garden.logger.get_data(event_type, "Control"), color='r', linestyle='--')
+            plt.title(event_type.value)
+        plt.show()
+    else:
+        # animate plant growth as circles
+        growth_animation = animation.ArtistAnimation(fig, frames, interval=300, blit=True, repeat_delay=1000)
+        plt.show()
+        growth_animation.save('simulation.mp4')
 
-    # animate plant growth as circles
-    # plt.figure()
-    # growth_animation = animation.ArtistAnimation(fig, frames, interval=300, blit=True, repeat_delay=1000)
-    # plt.show()
-    # growth_animation.save('simulation.mp4')
+def get_parsed_args():
+    parser = argparse.ArgumentParser(description='Run the garden simulation.')
+    parser.add_argument('--display', type=str, help='[a|p] Whether to show full animation [a] or just plots of plant behaviors [p]')
+    return parser.parse_args()
 
-run_simulation()
+args = get_parsed_args()
+run_simulation(args)
