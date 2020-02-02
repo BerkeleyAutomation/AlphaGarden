44a7825f18673ce6a83f496c44e54e2a4aacd72f
Kevin Li <kevintli98@gmail.com>
2020-02-01 20:39:22 -0800

    Add robot camera overlay with diversity/coverage stats

--- WebApp/src/Components/Element3.js
+++ WebApp/src/Components/Element3.js
@@ -9,6 +9,7 @@ import ZoomBox2 from '../Media/top-right-border.svg';
 import ZoomBox3 from '../Media/bottom-left-border.svg';
 import ZoomBox4 from '../Media/bottom-right-border.svg';
 import Grid from '../Media/grid.svg';
+import RobotCameraOverlay from './RobotCameraOverlay';
 // Component for dynamic zoom data display
 
 class Element3 extends React.Component{
@@ -236,6 +237,9 @@ class Element3 extends React.Component{
 	render(){
 	  return (
 	  		<div onMouseMove={this._onMouseMove.bind(this)}>
+
+				<RobotCameraOverlay />
+
 				<div id="Zoom_Container">
 					<img src={require("./Garden-Overview.bmp")} alt="Zaaa GARDEN" height="100%" width="100%" onClick={(e) => {console.log("???"); this.state.handleClick(e)}}  id={this.state.zoom}/>
 				</div>
