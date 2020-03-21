abcbc6b8950bcd48ec09a4fd2bffd5c77e85af56
William Wong <w07wong@yahoo.com>
2020-03-21 16:24:12 -0700

    more architectures

--- RL_Framework/constants.py
+++ RL_Framework/constants.py
@@ -11,14 +11,25 @@ class TrainingConstants(object):
     LR_STEP_SIZE = 1  # Epochs.
     LR_DECAY_RATE = 0.9
     LOG_INTERVAL = 1  # Batches.
-    DEVICE = DeviceConstants.CUDA
+    DEVICE = DeviceConstants.CPU
     OUTPUT_DIR = 'nets'
     NET_NAME = 'baseline_net'
     LOG_DIR = 'logs'
     NET_SAVE_FNAME = 'net.pth'
     NUM_CLASSES = 3
     ACT_DIM = NUM_CLASSES
-    FLAT_STATE_DIM = 18532 
+    # ORIGINAL
+    # FLAT_STATE_DIM = 18532
+    # REDUCED ORIGINAL
+    # FLAT_STATE_DIM = 5604
+    # ORIGINAL WITH POOLING
+    # FLAT_STATE_DIM = 8612
+    # REDUCED ORIGINAL WITH POOLING
+    # FLAT_STATE_DIM = 2564
+    # ORIGINAL WITH POOLING AND FC
+    # FLAT_STATE_DIM = 4452
+    # REDUCED WITH POOLING AND FC
+    # FLAT_STATE_DIM = 2564
     CC_IMG_DIMS = (3, 235, 499)
     CC_DS_DIMS = (3, 58, 124)
     RAW_DIMS = (12, 15, 30)
