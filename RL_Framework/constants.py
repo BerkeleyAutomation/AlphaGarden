464300c9c79738c986cb3cf97d93ec5b00c231fc
William Wong <w07wong@yahoo.com>
2020-03-20 19:41:55 -0700

    divide by zero and numpy to torch bug fixes

--- RL_Framework/constants.py
+++ RL_Framework/constants.py
@@ -3,22 +3,23 @@ class DeviceConstants(object):
     CPU = 'cpu'
 
 class TrainingConstants(object):
-    NUM_EPOCHS = 60
+    NUM_EPOCHS = 50
     TOTAL_SIZE = 1.0
     VAL_SIZE = 0.2
-    BSZ = 32
+    BSZ = 32 
     BASE_LR = 0.1
     LR_STEP_SIZE = 1  # Epochs.
     LR_DECAY_RATE = 0.9
     LOG_INTERVAL = 1  # Batches.
-    DEVICE = DeviceConstants.CUDA
+    DEVICE = DeviceConstants.CPU#DeviceConstants.CUDA
     OUTPUT_DIR = 'nets'
     NET_NAME = 'baseline_net'
     LOG_DIR = 'logs'
     NET_SAVE_FNAME = 'net.pth'
     NUM_CLASSES = 3
     ACT_DIM = NUM_CLASSES
-    FLAT_STATE_DIM = 115172 
-    CC_IMG_DIMS = (3, 58, 124)
+    FLAT_STATE_DIM = 5604
+    CC_IMG_DIMS = (3, 235, 499)
+    CC_DS_DIMS = (3, 58, 124)
     RAW_DIMS = (12, 15, 30)
     GLOBAL_CC_DIMS = (10, 1, 1)
