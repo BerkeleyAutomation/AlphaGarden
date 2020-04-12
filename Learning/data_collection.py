919f1d06d234efb852aa0db3e589600bb23da43c
Sebastian Oehme <sebastianoehme@MBP-SeOe.fritz.box>
2020-04-12 22:43:45 +0200

    * Added default path for generated data

--- Learning/data_collection.py
+++ Learning/data_collection.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     data_collection = DataCollection()
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', type=str)
+    parser.add_argument('-d', default='Generated_data', type=str)
     parser.add_argument('-s', type=int)
     args = parser.parse_args()
     params = vars(args)
