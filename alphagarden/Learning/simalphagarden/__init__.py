b9e4614c989fcd5ccf0859a36521644205c2be17
Sebastian Oehme <sebastian.oehme@tum.de>
2020-04-22 17:04:10 +0200

    Fix gym reregistering bug for repackaging as single package

--- alphagarden/Learning/simalphagarden/__init__.py
+++ alphagarden/Learning/simalphagarden/__init__.py
@@ -1,7 +1,11 @@
 from gym.envs.registration import register
+from gym.envs import registry
 import numpy as np
 
-register(
-    id='simalphagarden-v0',
-    entry_point='simalphagarden.envs:SimAlphaGardenEnv'
-)
+id = 'simalphagarden-v0'
+all_envs = [env_spec.id for env_spec in registry.all()]
+if id not in all_envs:
+   register(
+       id='simalphagarden-v0',
+       entry_point='simalphagarden.envs:SimAlphaGardenEnv'
+   )
