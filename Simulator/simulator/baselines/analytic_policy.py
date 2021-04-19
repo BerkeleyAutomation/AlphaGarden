bd5883cb318a4d3053923016fcdd213f167af805
William Wong <w07wong@yahoo.com>
2021-04-18 19:48:01 -0700

    Eval flag state analytic

--- Simulator/simulator/baselines/analytic_policy.py
+++ Simulator/simulator/baselines/analytic_policy.py
@@ -150,7 +150,7 @@ def policy(timestep, state, global_cc_vec, sector_rows, sector_cols, prune_windo
     """
     # Get rid of full garden observations
     if eval:
-        state = state[:-2]
+        state = state[:-1]
         plants_and_water = state
     else:
         plants_and_water = state[1]
