7646cb9904ee30818d61c0c2cc2f5a8860e73a20
Rishi Parikh <rishiparikh2001@gmail.com>
2021-07-24 15:08:35 -0700

    updating center const to remove unused plants

--- Center-Tracking/center_constants.py
+++ Center-Tracking/center_constants.py
@@ -8,28 +8,24 @@ IMAGE_NAME_PREFIX = "snc"
 COLORS = [
     (0, 0, 0),
     (255, 174, 0),
-    (255, 0, 0),
     (0, 124, 93),
-    (185, 180, 44),
-    (50, 50, 226),
     (50, 226, 174),
+    (50, 50, 226),
+    (185, 180, 44),
     (145, 50, 226),
-    (61, 123, 0),
     (226, 50, 170),
     (255, 85, 89)
 ]
 TYPES_TO_COLORS = {
-    "other":[0,0,0],
-    "arugula": [61, 123, 0],
-    "borage": [255, 174, 0],
-    "cilantro": [0, 124, 93],
+    "other": [0, 0, 0],
+    "borage": [255, 174, 1],
+    "cilantro": [1, 124, 93],
     "green-lettuce": [50, 226, 174],
-    "kale": [50, 50, 226],
-    "radicchio": [185, 180, 44],
+    "kale": [49, 49, 226],
+    "radiccio": [185, 180, 42],
     "red-lettuce": [145, 50, 226],
-    "sorrel": [255, 0, 0],
-    "swiss-chard": [226, 50, 170],
-    "turnip": [255, 85, 89]
+    "swiss-charge": [226, 50, 170],
+    "turnip": [254, 85, 89]
 }
 
 COLORS_TO_TYPES = {tuple(TYPES_TO_COLORS[k]) : k for k in TYPES_TO_COLORS}
