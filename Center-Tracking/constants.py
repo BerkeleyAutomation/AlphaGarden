bde4fbc7c29301f6fcc17104dbd6a74fb9bc74e2
Rishi Parikh <rishiparikh2001@gmail.com>
2021-07-24 15:07:43 -0700

    Updating constants.py to remove arugula and sorrel

--- Center-Tracking/constants.py
+++ Center-Tracking/constants.py
@@ -24,7 +24,7 @@ ACTIVATION_FN = 'softmax'
 
 LOSS_FN = 'weighted_jaccard'
 
-LOSS_WEIGHTS = [1, 1.5, 2, 1.5, 2, 3, 1.75, 1.25, 1.75, 3, 2.5]
+LOSS_WEIGHTS = [1, 2, 1.5, 2, 3, 1.75, 1.25, 3, 2.5]
 
 BASELINE_FILE = './final1.h5'
 # LAST_SAVED_MODEL = '{}/unet_'+BACKBONE+'_weighted_jaccard.h5'.format(MODEL_PATH)
@@ -34,70 +34,60 @@ CHECKPOINT_FILE = './final1.h5'
 
 TYPES_TO_COLORS = {
     "other": [0, 0, 0], #check
-    "arugula": [61, 123, 0], #check
     "borage": [255, 174, 0], #check
     "cilantro": [0, 124, 93], #check
     "green-lettuce": [50, 226, 174], #check
     "kale": [50, 50, 226], #check
     "radicchio": [185, 180, 44], #check
     "red-lettuce": [145, 50, 226], #check
-    "sorrel": [255, 0, 0], # check
     "swiss-chard": [226, 50, 170], #check
     "turnip": [254, 85, 89] #check
 }
 
 BINARY_ENCODINGS = {
-    "other": [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #check
-    "arugula": [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], #check
-    "borage": [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], #check
-    "cilantro": [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], #check
-    "green-lettuce": [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], #check
-    "kale": [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], #check
-    "radicchio": [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], #check
-    "red-lettuce": [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], #check
-    "sorrel": [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], #check
-    "swiss-chard": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], #check
-    "turnip": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] #check
+    "other": [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    "borage": [0, 1, 0, 0, 0, 0, 0, 0, 0],
+    "cilantro": [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    "green-lettuce": [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    "kale": [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    "radiccio": [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    "red-lettuce": [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    "swiss-charge": [0, 0, 0, 0, 0, 0, 0, 1, 0],
+    "turnip": [0, 0, 0, 0, 0, 0, 0, 0, 1]
 }
 
 LABEL_ENC = {
     "other": 0, #check
-    "arugula": 1, #check
-    "borage": 2, #check
-    "cilantro": 3, #check
-    "green-lettuce": 4, #check
-    "kale": 5, #check
-    "radicchio": 6, #check
-    "red-lettuce": 7, #check
-    "sorrel": 8, #check
-    "swiss-chard": 9, #check
-    "turnip": 10 #check
+    "borage": 1, #check
+    "cilantro": 2, #check
+    "green-lettuce": 3, #check
+    "kale": 4, #check
+    "radicchio": 5, #check
+    "red-lettuce": 6, #check
+    "swiss-chard": 7, #check
+    "turnip": 8 #check
 }
 
 COLORS = [
-    (0, 0, 0), #check
-    (61, 123, 0), #check
-    (255, 174, 0), #check
-    (0, 124, 93), #check
-    (50, 226, 174), #check
-    (50, 50, 226), #check
-    (185, 180, 44), #check
-    (145, 50, 226), #check
-    (255, 0, 0), #check
-    (226, 50, 170), #check
-    (254, 85, 89) #check
+    (0, 0, 0),
+    (255, 174, 0),
+    (0, 124, 93),
+    (50, 226, 174),
+    (50, 50, 226),
+    (185, 180, 44),
+    (145, 50, 226),
+    (226, 50, 170),
+    (255, 85, 89)
 ]
 
 TYPES = [
     "other",
-    "arugula",
     "borage",
     "cilantro",
     "green-lettuce",
     "kale",
     "radicchio",
     "red-lettuce",
-    "sorrel",
     "swiss-chard",
     "turnip"
 ]
@@ -112,7 +102,7 @@ IOU_EVAL_FILE = 'iou_eval_file.csv'
 TEST_MODEL = './models/model_3_16.h5'
 
 GARDEN_DATE_YEAR = 2021
-GARDEN_DATE_MONTH = 5
+GARDEN_DATE_MONTH = 7
 GARDEN_DATE_DAY = 1
 
 # SHIFT = 'confidence'
